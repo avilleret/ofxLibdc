e80ef1215060701074570872b8dab55609a9c307
Antoine Villeret <antoine.villeret@gmail.com>
2016-10-31 16:06:09 +0100

    indentation

--- src/Camera.h
+++ src/Camera.h
@@ -13,155 +13,155 @@ namespace ofxLibdc {
 
 class Camera {
 public:
-	Camera(bool isStereoCamera = false);
-	static int getCameraCount();
-	
-	// pre-setup settings
-	void setSize(unsigned int width, unsigned int height);
-	void setPosition(unsigned int roiLeft, unsigned int roiTop);
-	void setImageType(ofImageType imageType);
-	void setFormat7(bool useFormat7, int mode = 0);
-	void set1394b(bool use1394b);
-	void setBlocking(bool blocking);
-	void setBayerMode(dc1394color_filter_t bayerMode);
-	void setFrameRate(float frameRate);
-	
-	ofImageType getImageType() const;
-	bool getBlocking() const;
-	unsigned int getWidth() const;
-	unsigned int getHeight() const;
-	float getFrameRate() const;
-    
+    Camera(bool isStereoCamera = false);
+    static int getCameraCount();
+
+    // pre-setup settings
+    void setSize(unsigned int width, unsigned int height);
+    void setPosition(unsigned int roiLeft, unsigned int roiTop);
+    void setImageType(ofImageType imageType);
+    void setFormat7(bool useFormat7, int mode = 0);
+    void set1394b(bool use1394b);
+    void setBlocking(bool blocking);
+    void setBayerMode(dc1394color_filter_t bayerMode);
+    void setFrameRate(float frameRate);
+
+    ofImageType getImageType() const;
+    bool getBlocking() const;
+    unsigned int getWidth() const;
+    unsigned int getHeight() const;
+    float getFrameRate() const;
+
     // stereo camera settings
     void setStereoCamera(bool isStereo);
     bool isStereoCamera() const;
-	
-	virtual bool setup(int cameraNumber = 0);
-	virtual bool setup(string cameraGuid);
-	virtual ~Camera();
-	
-	// post-setup settings	
-	
-	// normalized values
-	void setBrightness(float brightness);
-	void setGamma(float gamma);
-	void setGain(float gain);
-	void setExposure(float exposure);
-	void setShutter(float shutter);
-	void setFeature(dc1394feature_t feature, float value);
-	
-	// absolute values
-	void setBrightnessAbs(float brightness);
-	void setGammaAbs(float gamma);
-	void setGainAbs(float gain);
-	void setExposureAbs(float exposure);
-	void setShutterAbs(float shutter); // in seconds
-	void setFeatureAbs(dc1394feature_t feature, float value);
-	
-	// raw values
-	void setBrightnessRaw(unsigned int brightness);
-	void setGammaRaw(unsigned int gamma);
-	void setGainRaw(unsigned int gain);
-	void setExposureRaw(unsigned int exposure);
-	void setShutterRaw(unsigned int shutter);
-	void setFeatureRaw(dc1394feature_t feature, unsigned int value);
-	
-	// normalized values
-	float getBrightness() const;
-	float getGamma() const;
-	float getGain() const;
-	float getExposure() const;
-	float getShutter() const;
-	float getFeature(dc1394feature_t feature) const;
-	
-	// absolute values
-	float getBrightnessAbs() const;
-	float getGammaAbs() const;
-	float getGainAbs() const;
-	float getExposureAbs() const;
-	float getShutterAbs() const; // in seconds
-	float getFeatureAbs(dc1394feature_t feature) const;
-	
-	// raw values
-	unsigned int getBrightnessRaw() const;
-	unsigned int getGammaRaw() const;
-	unsigned int getGainRaw() const;
-	unsigned int getExposureRaw() const;
-	unsigned int getShutterRaw() const;
-	unsigned int getFeatureRaw(dc1394feature_t feature) const;
-	
-	// raw value ranges
-	void getBrightnessRawRange(unsigned int* min, unsigned int* max) const;
-	void getGammaRawRange(unsigned int* min, unsigned int* max) const;
-	void getGainRawRange(unsigned int* min, unsigned int* max) const;
-	void getExposureRawRange(unsigned int* min, unsigned int* max) const;
-	void getShutterRawRange(unsigned int* min, unsigned int* max) const;
-	void getFeatureRawRange(dc1394feature_t feature, unsigned int* min, unsigned int* max) const;
-	
-	// abs value ranges
-	void getBrightnessAbsRange(float* min, float* max) const;
-	void getGammaAbsRange(float* min, float* max) const;
-	void getGainAbsRange(float* min, float* max) const;
-	void getExposureAbsRange(float* min, float* max) const;
-	void getShutterAbsRange(float* min, float* max) const; // in seconds
-	void getFeatureAbsRange(dc1394feature_t feature, float* min, float* max) const;
-	
-	void printFeatures() const;
-	
-	// image grabbing
-	
-	bool grabStill(ofImage& img);
+
+    virtual bool setup(int cameraNumber = 0);
+    virtual bool setup(string cameraGuid);
+    virtual ~Camera();
+
+    // post-setup settings
+
+    // normalized values
+    void setBrightness(float brightness);
+    void setGamma(float gamma);
+    void setGain(float gain);
+    void setExposure(float exposure);
+    void setShutter(float shutter);
+    void setFeature(dc1394feature_t feature, float value);
+
+    // absolute values
+    void setBrightnessAbs(float brightness);
+    void setGammaAbs(float gamma);
+    void setGainAbs(float gain);
+    void setExposureAbs(float exposure);
+    void setShutterAbs(float shutter); // in seconds
+    void setFeatureAbs(dc1394feature_t feature, float value);
+
+    // raw values
+    void setBrightnessRaw(unsigned int brightness);
+    void setGammaRaw(unsigned int gamma);
+    void setGainRaw(unsigned int gain);
+    void setExposureRaw(unsigned int exposure);
+    void setShutterRaw(unsigned int shutter);
+    void setFeatureRaw(dc1394feature_t feature, unsigned int value);
+
+    // normalized values
+    float getBrightness() const;
+    float getGamma() const;
+    float getGain() const;
+    float getExposure() const;
+    float getShutter() const;
+    float getFeature(dc1394feature_t feature) const;
+
+    // absolute values
+    float getBrightnessAbs() const;
+    float getGammaAbs() const;
+    float getGainAbs() const;
+    float getExposureAbs() const;
+    float getShutterAbs() const; // in seconds
+    float getFeatureAbs(dc1394feature_t feature) const;
+
+    // raw values
+    unsigned int getBrightnessRaw() const;
+    unsigned int getGammaRaw() const;
+    unsigned int getGainRaw() const;
+    unsigned int getExposureRaw() const;
+    unsigned int getShutterRaw() const;
+    unsigned int getFeatureRaw(dc1394feature_t feature) const;
+
+    // raw value ranges
+    void getBrightnessRawRange(unsigned int* min, unsigned int* max) const;
+    void getGammaRawRange(unsigned int* min, unsigned int* max) const;
+    void getGainRawRange(unsigned int* min, unsigned int* max) const;
+    void getExposureRawRange(unsigned int* min, unsigned int* max) const;
+    void getShutterRawRange(unsigned int* min, unsigned int* max) const;
+    void getFeatureRawRange(dc1394feature_t feature, unsigned int* min, unsigned int* max) const;
+
+    // abs value ranges
+    void getBrightnessAbsRange(float* min, float* max) const;
+    void getGammaAbsRange(float* min, float* max) const;
+    void getGainAbsRange(float* min, float* max) const;
+    void getExposureAbsRange(float* min, float* max) const;
+    void getShutterAbsRange(float* min, float* max) const; // in seconds
+    void getFeatureAbsRange(dc1394feature_t feature, float* min, float* max) const;
+
+    void printFeatures() const;
+
+    // image grabbing
+
+    bool grabStill(ofImage& img);
     bool grabStill(ofImage& img1, ofImage& img2);
-	bool grabVideo(ofImage& img, bool dropFrames = true);
+    bool grabVideo(ofImage& img, bool dropFrames = true);
     bool grabVideo(ofImage& img1, ofImage& img2, bool dropFrames = true);
-	
-	void flushBuffer();
-	
-	dc1394camera_t* getLibdcCamera();
-	bool isReady() const;
+
+    void flushBuffer();
+
+    dc1394camera_t* getLibdcCamera();
+    bool isReady() const;
 
 protected:
-	static dc1394_t* libdcContext;
-	static int libdcCameras;
-	static void startLibdcContext();
-	static void stopLibdcContext();
-    
+    static dc1394_t* libdcContext;
+    static int libdcCameras;
+    static void startLibdcContext();
+    static void stopLibdcContext();
+
     bool isStereo;
     dc1394stereo_method_t stereoMethod;
     dc1394color_coding_t colorCoding;
     dc1394bayer_method_t bayerMethod;
-	
-	static ofImageType getOfImageType(dc1394color_coding_t imageType);
-	static dc1394color_coding_t getLibdcType(ofImageType imageType);
-		
-	dc1394camera_t* camera;
-	dc1394video_mode_t videoMode;
-	dc1394capture_policy_t capturePolicy;
-	unsigned int width, height, left, top;
-	ofImageType imageType;
-	float frameRate;
-	
-	bool useBayer;
-	dc1394color_filter_t bayerMode;
-	
-	bool useFormat7;
-	int format7Mode;
-	bool use1394b;
-	bool ready;
-	
-	bool grabFrame(ofImage& img);
+
+    static ofImageType getOfImageType(dc1394color_coding_t imageType);
+    static dc1394color_coding_t getLibdcType(ofImageType imageType);
+
+    dc1394camera_t* camera;
+    dc1394video_mode_t videoMode;
+    dc1394capture_policy_t capturePolicy;
+    unsigned int width, height, left, top;
+    ofImageType imageType;
+    float frameRate;
+
+    bool useBayer;
+    dc1394color_filter_t bayerMode;
+
+    bool useFormat7;
+    int format7Mode;
+    bool use1394b;
+    bool ready;
+
+    bool grabFrame(ofImage& img);
     bool grabFrame(ofImage& img1, ofImage& img2);
-	bool initCamera(uint64_t cameraGuid);
-	bool applySettings();
-	
-	void quantizeSize();
-	void quantizePosition();
-	
-	void setTransmit(bool transmit);
-	unsigned int getSourceDepth() const;
-	
-	static string makeString(int name);
-	static float makeFloat(int name);
+    bool initCamera(uint64_t cameraGuid);
+    bool applySettings();
+
+    void quantizeSize();
+    void quantizePosition();
+
+    void setTransmit(bool transmit);
+    unsigned int getSourceDepth() const;
+
+    static string makeString(int name);
+    static float makeFloat(int name);
 };
 
 }
--- src/Grabber.cpp
+++ src/Grabber.cpp
@@ -3,15 +3,15 @@
 namespace ofxLibdc {
 
 Grabber::Grabber() :
-	newFrame(false) {
+    newFrame(false) {
 }
 
 ofTexture& Grabber::getTextureReference() {
-	return buffer.getTexture();
+    return buffer.getTexture();
 }
 
 void Grabber::setUseTexture(bool useTexture) {
-	buffer.setUseTexture(useTexture);
+    buffer.setUseTexture(useTexture);
 }
 
 //unsigned char* Grabber::getPixels() {
@@ -19,50 +19,50 @@ void Grabber::setUseTexture(bool useTexture) {
 //}
 
 ofImage& Grabber::getBuffer() {
-	return buffer;
+    return buffer;
 }
 
 void Grabber::update() {
-	grabFrame();
+    grabFrame();
 }
 
 void Grabber::grabFrame() {
-	newFrame = grabVideo(buffer);
+    newFrame = grabVideo(buffer);
     if (newFrame) buffer.update();
 }
 
 void Grabber::draw(float x, float y) {
-	buffer.draw(x, y);
+    buffer.draw(x, y);
 }
 
 void Grabber::draw(float x, float y, float w, float h) {
-	buffer.draw(x, y, w, h);
+    buffer.draw(x, y, w, h);
 }
 
 float Grabber::getWidth() {
-	return Camera::getWidth();
+    return Camera::getWidth();
 }
 
 float Grabber::getHeight() {
-	return Camera::getHeight();
+    return Camera::getHeight();
 }
 
 bool Grabber::isFrameNew() const {
-	if(newFrame) {
-		newFrame = false;
-		return true;
-	}
-	return false;
+    if(newFrame) {
+        newFrame = false;
+        return true;
+    }
+    return false;
 }
 
 void Grabber::close() {
-	// handled automatically by ~Camera
+    // handled automatically by ~Camera
 }
 
 ofPixels& Grabber::getPixels() {
-	return buffer.getPixels();
+    return buffer.getPixels();
 }
-    
+
 const ofPixels& Grabber::getPixels() const {
     return buffer.getPixels();
 }
